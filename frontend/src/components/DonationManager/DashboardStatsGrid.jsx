8bdc78a15bf81b516151592ca5b4836cc6072ccd
chamodi piyadasa <chamodipiyadasa@gmail.com>
2024-04-03 12:21:24 +0530

    dashboard

--- frontend/src/components/DonationManager/DashboardStatsGrid.jsx
+++ frontend/src/components/DonationManager/DashboardStatsGrid.jsx
@@ -10,26 +10,21 @@ export default function DashboardStatsGrid() {
 			<BoxWrapper>
 				<div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
 					<IoPeople className="text-2xl text-white" />
-				</div>
-				<div className="pl-4">
-					<span className="text-sm text-gray-500 font-light">Total Rescue Request</span>
-					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">60</strong>
-						
-					</div>
-				</div>
+					
+			
+			</div>
+			<iframe  src="https://charts.mongodb.com/charts-test-tjqsf/embed/charts?id=660cf599-cd05-4408-8365-959223b483ef&maxDataAge=60&theme=light&autoRefresh=true"></iframe>
 			</BoxWrapper>
+
 			<BoxWrapper>
 				<div className="rounded-full h-12 w-12 flex items-center justify-center bg-orange-600">
 					<FaTasks className="text-2xl text-white" />
 				</div>
-				<div className="pl-4">
-					<span className="text-sm text-gray-500 font-light">Total Tasks</span>
-					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">50</strong>
-					</div>
-				</div>
+			
+				<iframe src="https://charts.mongodb.com/charts-test-tjqsf/embed/charts?id=660cf96e-cd05-4880-8df2-9592230d5617&maxDataAge=60&theme=light&autoRefresh=true"></iframe>
 			</BoxWrapper>
+
+			
 			<BoxWrapper>
 				<div className="rounded-full h-12 w-12 flex items-center justify-center bg-yellow-400">
 					<MdOutlinePets className="text-2xl text-white" />
@@ -42,17 +37,7 @@ export default function DashboardStatsGrid() {
 					</div>
 				</div>
 			</BoxWrapper>
-			<BoxWrapper>
-				<div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
-					<FaHome className="text-2xl text-white" />
-				</div>
-				<div className="pl-4">
-					<span className="text-sm text-gray-500 font-light">Adopted</span>
-					<div className="flex items-center">
-						<strong className="text-xl text-gray-700 font-semibold">2</strong>
-					</div>
-				</div>
-			</BoxWrapper>
+			
 		</div>
 		</div>
 	)
