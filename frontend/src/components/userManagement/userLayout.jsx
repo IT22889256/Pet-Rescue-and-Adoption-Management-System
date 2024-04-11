f2d6d7672a232f3158b81e3c6e89e31258f2b45b
Dhanuka Chamalitha <dhanukawickramasinghe1024@gmail.com>
2024-04-12 02:43:56 +0530

    dev

--- frontend/src/components/userManagement/userLayout.jsx
+++ frontend/src/components/userManagement/userLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../shared/Header";
-import UserSideBar from "./UserSideBar";
+import UserSideBar from "./userSideBar";
 
 export default function userLayout() {
   return (
