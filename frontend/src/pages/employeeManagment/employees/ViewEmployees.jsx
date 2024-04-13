c8c361316df51456d8d267d18f5898280300e8d7
induwaraR98 <icredu98@gmail.com>
2024-04-13 15:48:11 +0530

    create upload images and view images

--- frontend/src/pages/employeeManagment/employees/ViewEmployees.jsx
+++ frontend/src/pages/employeeManagment/employees/ViewEmployees.jsx
@@ -48,8 +48,8 @@ export default function ViewEmployee() {
                     <h3 className="text-lg font-medium leading-6 text-gray-900 text-center">Employee Profile</h3>
                 </div>
                 <div className="mt-3 flex text-xs justify-center">
-                    <img className='object-cover h-60 w-60 m-5 rounded-full' src="https://i.ibb.co/713YjHp/pexels-emrah-ayvali-1981111.jpg" alt='profile_Image'/>
-                </div>
+                <img className='object-cover h-60 w-60 m-5 rounded-full' src={employee.employeeimgUrl} alt='profile_Image'/>
+				</div>
                 <div className="border-t border-gray-200">
                     <dl>
 					<div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
