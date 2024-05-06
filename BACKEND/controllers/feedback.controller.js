783e945b6a08c483e3b2bd899a88043ccb587c47
Nisfa2002 <nisfanasardeen2002@gmail.com>
2024-05-06 19:30:03 +0530

    uf Manager

--- BACKEND/controllers/feedback.controller.js
+++ BACKEND/controllers/feedback.controller.js
@@ -1,9 +1,5 @@
 const Feedback = require("../modules/feedback.model");
-<<<<<<< HEAD
-const Counter = require('../modules/counter.model')
-=======
 const Counter = require("../modules/counter.model");
->>>>>>> dac0b87a7f754763c50d5d7d27d7b43fbb9b3968
 
 const getFeedbacks = async (req, res) => {
   try {
@@ -27,16 +23,6 @@ const getFeedback = async (req, res) => {
 const createFeedback = async (req, res) => {
   try {
     const counter = await Counter.findByIdAndUpdate(
-<<<<<<< HEAD
-      { _id: 'feedbackId' },
-      { $inc: { seq: 1 } },
-      { new: true, upsert: true }
-    );
-  
-    const feedbackId = 'FB' + String(counter.seq).padStart(3, '0');
-
-    const feedback = await Feedback.create({...req.body,feedbackId:feedbackId});
-=======
       { _id: "feedbackId" },
       { $inc: { seq: 1 } },
       { new: true, upsert: true }
@@ -48,7 +34,6 @@ const createFeedback = async (req, res) => {
       ...req.body,
       feedbackId: feedbackId,
     });
->>>>>>> dac0b87a7f754763c50d5d7d27d7b43fbb9b3968
     res.status(200).json(feedback);
   } catch (error) {
     res.status(500).json({ message: error.message });
